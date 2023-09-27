c1f32b9cc2467edde243882b93909317cecf6880
MelCagape4 <melanio.cagape@collaberadigital.com>
2023-09-27 15:26:40 +0800

    added comments

--- src/Web/Constants.cs
+++ src/Web/Constants.cs
@@ -1,4 +1,6 @@
-﻿namespace Microsoft.eShopWeb.Web;
+﻿// My 3rd commit
+
+namespace Microsoft.eShopWeb.Web;
 
 public static class Constants
 {
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,4 @@
-﻿// My first change
+﻿// My 2nd change
 
 using System.Net.Mime;
 using Ardalis.ListStartupServices;
